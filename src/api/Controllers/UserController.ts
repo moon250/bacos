4256a48cdc548379c364a0afed74eed80b84e39d
moon250 <mooneupho@gmail.com>
2023-10-04 15:47:37 +0200

    feat: don't re-create a temporary account if user already have one

--- src/api/Controllers/UserController.ts
+++ src/api/Controllers/UserController.ts
@@ -2,24 +2,44 @@ import { JsonResponse } from "../Router/JsonResponse.js";
 import { client } from "../Redis/Client.js";
 import { FastifyRequest } from "fastify";
 
-export const UserController =  {
+type User = {
+  username: string;
+  ip: string;
+};
+
+export const UserController = {
   async create(request: FastifyRequest): Promise<JsonResponse> {
-    const body = request.body as { [key: string]: unknown }
-    const ip = request.ip
+    const ip = request.ip;
+    const body = request.body as { [key: string]: unknown };
+    const username = body["username"];
 
-    await client.set(`users:${ip}`, {
-      body: body["username"],
-      ip
-    },
-  10)
+    if (await client.exists(`users:${ip}`)) {
+      const user = (await client.get(`users:${ip}`)) as User;
 
-    return new JsonResponse({'message': 'Temporary user successfully created'})
+      return new JsonResponse({
+        message: `You already have a temporary user. Done nothing.`,
+        username: user.username,
+      });
+    }
+
+    await client.set(
+      `users:${ip}`,
+      {
+        username,
+        ip,
+      } as User,
+      6 * 60 * 60, // 6h
+    );
+
+    return new JsonResponse({
+      message: `Temporary user '${username}' successfully created`,
+    });
   },
 
   create_validation: {
     properties: {
-      username: { type: 'string' }
+      username: { type: "string" },
     },
-    required: ['username']
-  }
-}
+    required: ["username"],
+  },
+};
