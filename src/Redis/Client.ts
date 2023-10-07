b3d00ed00389ded5a5315fe1e3cb5825407c698e
moon250 <mooneupho@gmail.com>
2023-10-07 13:47:24 +0200

    ref: add sets and hashs related redis methods

--- src/Redis/Client.ts
+++ src/Redis/Client.ts
@@ -1,23 +1,51 @@
 import { connection } from "./Connection.js";
 
-const get = async <T = any>(key: string): Promise<T> => {
-  return JSON.parse((await connection.get(key)) as string);
-};
-
-const set = async (key: string, value: any, ttl: number | null = null) => {
-  const params: { EX?: number } = {};
-
-  if (ttl !== null) {
-    params["EX"] = ttl;
-  }
-
-  await connection.set(key, JSON.stringify(value), params);
-};
-
-const exists = async (key: string) => !!(await connection.exists(key));
-
 export const client = {
-  get,
-  set,
-  exists,
+  get: async <T = any>(key: string): Promise<T> => {
+    return JSON.parse((await connection.get(key)) as string);
+  },
+
+  set: async (key: string, value: any, ttl: number | null = null) => {
+    const params: { EX?: number } = {};
+
+    if (ttl !== null) {
+      params["EX"] = ttl;
+    }
+
+    await connection.set(key, JSON.stringify(value), params);
+  },
+
+  exists: async (key: string) => !!(await connection.exists(key)),
+
+  sAdd: async (key: string, ...values: any[]) => {
+    for (const value of values) {
+      await connection.sAdd(key, value);
+    }
+  },
+
+  sRem: async (key: string, ...values: any[]) => {
+    for (const value of values) {
+      await connection.sRem(key, value);
+    }
+  },
+
+  sIsMember: async (key: string, value: any): Promise<boolean> => {
+    return await connection.sIsMember(key, value);
+  },
+
+  hSet: async (key: string, field: string, value: any) => {
+    await connection.hSet(key, field, value);
+  },
+
+  hGet: async (key: string, field: string): Promise<string> => {
+    return (await connection.hGet(key, field)) as string;
+  },
+
+  hExists: async (key: string, field: string) => {
+    return await connection.hExists(key, field);
+  },
+
+  hDel: async (key: string, ...fields: string[]) => {
+    await connection.hDel(key, fields);
+  },
 };
