f8457b0e05a7734ebce2e7ee3b6220ff14f6986a
moon250 <mooneupho@gmail.com>
2023-10-11 18:26:16 +0200

    feat: add the form to submit the username

--- site/src/Components/Pages/HomePage.vue
+++ site/src/Components/Pages/HomePage.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home-page">
-    <div class="title">
+    <div class="home-page__title">
       <h1>
         Bacos <Icon name="book" :icon-class="false" class="title__book" />
       </h1>
       <h2>Le jeu du petit bac</h2>
+    </div>
+    <div class="home-page__form">
+      <input type="text" name="" id="" placeholder="Entrez votre pseudo" />
+      <button>Jouer</button>
     </div>
   </div>
 </template>
@@ -16,11 +20,14 @@ import Icon from "../Icon.vue";
 <style scoped>
 .home-page {
   height: 100dvh;
-  display: grid;
-  place-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
 }
 
-.title {
+.home-page__title {
   display: grid;
   place-items: center;
 
@@ -35,13 +42,18 @@ import Icon from "../Icon.vue";
   width: 100px;
 }
 
+.home-page__form {
+  display: flex;
+  gap: 16px;
+}
+
 @media screen and (max-width: 600px) {
   .title__book {
     height: 75px;
     width: 75px;
   }
 
-  .title {
+  .home-page__title {
     h1 {
       gap: 1rem;
     }
