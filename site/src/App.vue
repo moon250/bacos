92ae7cdd386c4e67552e21aaa4a109afdda987da
moon250 <mooneupho@gmail.com>
2023-10-11 18:33:48 +0200

    feat: add footer and responsive on forms

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <HomePage />
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import HomePage from "./Components/Pages/HomePage.vue";
+import Footer from "./Components/Footer.vue";
 </script>
 
 <style scoped></style>
---
+++ site/src/Components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Fait avec ♥️ par <a href="https://github.com/moon250">moon250</a></p>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+p {
+  font-size: 0.9rem;
+  position: absolute;
+  width: max-content;
+  bottom: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
