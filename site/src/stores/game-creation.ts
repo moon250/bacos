0f55475ab9bb29e12e7dc5003dd8f723a5e34394
moon250 <mooneupho@gmail.com>
2023-10-21 15:02:57 +0200

    feat: add default excluded letters

--- site/src/stores/game-creation.ts
+++ site/src/stores/game-creation.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useGameCreationStore = defineStore("game-creation", () => {
-  const excludedLetters = ref<string[]>([]);
+  const excludedLetters = ref<string[]>(["W", "X", "Y", "Z"]);
   const lettersCount = ref(5);
 
   return { excludedLetters, lettersCount };
